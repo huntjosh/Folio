94f6202a98c9e59f06671abe8aa3e3bff2421901
Josh Hunt <joshhhunt@gmail.com>
2017-10-23 15:40:55 +1300

    Added class builders and switched to hashsets

--- src/Portfolio/Asset/Asset.java
+++ src/Portfolio/Asset/Asset.java
@@ -1,9 +1,77 @@
 package Portfolio.Asset;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import Task.Task;
 
 public class Asset {
-    private ArrayList<Task> tasks;
+    private String name;
+    private double value;
+    private HashSet<Task> tasks;
 
+    public static class Builder{
+        private final String name;
+        private final double value;
+        private HashSet<Task> tasks = new HashSet<Task>();
+
+        public Builder(String name, double value){
+            this.value = value;
+            this.name = name;
+        }
+
+        public Builder task(Task task)
+        {
+            if (!this.tasks.add(task)) throw new IllegalStateException("Unable to add task: " + task.getName());
+
+            return this;
+        }
+
+        public Builder tasks(HashSet<Task> tasks)
+        { this.tasks = tasks;     return this; }
+
+        public Asset build(){
+            return new Asset(this);
+        }
+    }
+
+    public Asset(Builder builder){
+        this.name = builder.name;
+        this.value = builder.value;
+        this.tasks = new HashSet<>();
+
+        // Now that the Asset is being created, we can add the link from the Task to this Asset
+        for (Task task : builder.tasks){ linkToTask(task); }
+    }
+
+    public double getValue(){ return value; }
+
+    public void setValue(double value){ this.value = value; }
+
+    public String getName(){ return name; }
+
+    public void setName(String name){ this.name = name; }
+
+    public HashSet<Task> getTasks(){ return new HashSet<>(tasks); }
+
+    public boolean addTask(Task task){ return tasks.add(task); }
+
+    public boolean linkToTask(Task task){
+        if (addTask(task)) {
+            task.addAsset(this);
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Task> addTasks(HashSet<Task> tasks){
+        // Could potentially have duplicate tasks, so need an array list
+        ArrayList<Task> failedTasks = new ArrayList<Task>();
+
+        for (Task task : tasks){
+            if (!addTask(task)) failedTasks.add(task);
+            else task.addAsset(this);
+        }
+
+        return failedTasks;
+    }
 }
--- src/Task/Task.java
+++ src/Task/Task.java
@@ -1,6 +1,9 @@
 package Task;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import Portfolio.Asset.Asset;
 import User.User;
 
 public class Task {
@@ -8,54 +11,98 @@ public class Task {
     private LocalDateTime createdAt;
     private String name;
     private User assignee;
+    private HashSet<Asset> assets;
 
-    public Task(LocalDateTime due, LocalDateTime createdAt, String name){
-        this.due = due;
-        this.createdAt = createdAt;
-        this.name = name;
-        this.assignee = null;
+    public static class Builder{
+        private final String name;
+        private final LocalDateTime createdAt;
+        private final LocalDateTime due;
+
+        private User user = null;
+        private HashSet<Asset> assets = new HashSet<>();
+
+        public Builder(LocalDateTime createdAt, LocalDateTime due, String name){
+            this.createdAt = createdAt;
+            this.due = due;
+            this.name = name;
+        }
+
+        public Builder user(User user)
+        { this.user = user;     return this; }
+
+        public Builder assets(HashSet<Asset> assets)
+        { this.assets = assets;     return this; }
+
+        public Task build(){
+            return new Task(this);
+        }
     }
 
-    public Task(LocalDateTime due, LocalDateTime createdAt, String name, User user){
-        this.due = due;
-        this.createdAt = createdAt;
-        this.name = name;
-        this.assignee = user;
+    public Task(Builder builder){
+        this.due = builder.due;
+        this.createdAt = builder.createdAt;
+        this.name = builder.name;
+        this.assignee = builder.user;
+        this.assets = builder.assets;
     }
 
-    public void unassign(){
+    public boolean addAsset(Asset asset){ return assets.add(asset); }
+
+    public boolean linkToAsset(Asset asset){
+        if (addAsset(asset)) {
+            asset.addTask(this);
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Asset> addAssets(HashSet<Asset> assets){
+        // Could potentially have duplicate tasks, so need an array list
+        ArrayList<Asset> failedAsset = new ArrayList<>();
+
+        for (Asset asset : assets){ if (!addAsset(asset)) failedAsset.add(asset); }
+
+        return failedAsset;
+    }
+
+    public boolean unassign(){
+        if (!hasAssignee()) return false;
+
         assignee = null;
+        return true;
     }
 
-    public void assign(User user){
+    public boolean assign(User user){
+        if (hasAssignee()) return false;
+
         assignee = user;
+        return true;
     }
 
-    public User getAssignee(){
-        return this.assignee;
-    }
+    public User getAssignee(){ return this.assignee; }
 
-    public boolean hasAssignee(){
-        return this.assignee != null;
-    }
+    public boolean hasAssignee(){ return this.assignee != null; }
+
+    public boolean hasAsset(){ return assets.size() > 0; }
 
     public LocalDateTime getCreatedAt() {
-        return createdAt;
+        return LocalDateTime.of(createdAt.toLocalDate(), createdAt.toLocalTime());
     }
 
-    public LocalDateTime getDue() {
-        return due;
+    public LocalDateTime getDue(){
+        return LocalDateTime.of(due.toLocalDate(), due.toLocalTime());
     }
 
-    public void setDue(LocalDateTime due) {
-        this.due = due;
-    }
+    public void setDue(LocalDateTime due) { this.due = due; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
+
+    public ArrayList<Asset> getAssets(){ return new ArrayList<>(assets); }
+
+//    public Task duplicate(){
+//        //  We want to return the same task, but with no assignee
+//        return new Task(getDue(), getCreatedAt(), getName());
+//    }
 }
